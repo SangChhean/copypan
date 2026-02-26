827c55a6b63387dd21c4a90062aad39d179ae3f6
SangChhean <praiselei16017@gmail.com>
2026-02-26 12:14:31 +0700

    升级 ES 客户端至 8.x，更新连接配置

--- back_mic/backend/es_config.py
+++ back_mic/backend/es_config.py
@@ -3,12 +3,22 @@ Elasticsearch 统一配置
 修改 ES 连接地址只需改此文件中的 ES_HOSTS
 """
 from elasticsearch import Elasticsearch
+import os
+from dotenv import load_dotenv
 
-# ES 连接地址，后续修改只需改此处
-ES_HOSTS = ["http://localhost:9200"]
+load_dotenv()
+
+ES_HOST = os.getenv("ES_HOST", "localhost")
+ES_PORT = os.getenv("ES_PORT", "9200")
+ES_USERNAME = os.getenv("ES_USERNAME", "elastic")
+ES_PASSWORD = os.getenv("ES_PASSWORD", "")
 
 # 请求超时（秒），建索引、删索引等操作可能较慢，默认 10 秒易超时
 ES_REQUEST_TIMEOUT = 60
 
 # 全局 ES 客户端实例
-es = Elasticsearch(hosts=ES_HOSTS, request_timeout=ES_REQUEST_TIMEOUT)
+es = Elasticsearch(
+    hosts=[f"http://{ES_HOST}:{ES_PORT}"],
+    basic_auth=(ES_USERNAME, ES_PASSWORD),
+    request_timeout=ES_REQUEST_TIMEOUT
+)
