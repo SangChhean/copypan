db6e20a4262ef52463ec1df26cddc3121eb7efa3
Piseth <liupiseth@gmail.com>
2026-06-10 21:04:38 +0700

    新作毛胚纲目功能

--- front_mic/frontend/src/components/toolbox/ToolBox.vue
+++ front_mic/frontend/src/components/toolbox/ToolBox.vue
@@ -137,6 +137,9 @@ const openZh2tw = () => {
         <a-card-grid class="card c2" @click="go('/feast-outline-b')">
           <span class="card_text">节期纲目测试</span>
         </a-card-grid>
+        <a-card-grid class="card c2" @click="go('/rough-outline-b')">
+          <span class="card_text">毛胚纲目测试</span>
+        </a-card-grid>
       </a-card>
     </div>
     <br />
